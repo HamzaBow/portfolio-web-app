0cfb314eed6a9449ddc0494863a415cef8e7be18
Hamza Bow <hamzabow77@gmail.com>
2022-07-29 11:37:18 +0100

    use webpack for all HTML pages

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,29 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: "development",
   entry: {
-    bundle: path.resolve(__dirname, 'src/index.js'),
+    bundle: path.resolve(__dirname, "src/index.js"),
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
   },
-  plugins: [new HtmlWebpackPlugin({
-    title: 'Wet Back',
-    filename: 'index.html',
-    template: 'src/index.html',
-  })],
-}
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Web Pack",
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+
+    new HtmlWebpackPlugin({
+      title: "Web Pack",
+      filename: "mathnote-project.html",
+      template: "src/mathnote-project.html",
+    }),
+
+    new HtmlWebpackPlugin({
+      title: "Web Pack",
+      filename: "project-semantic-search.html",
+      template: "src/project-semantic-search.html",
+    }),
+  ],
+};
